024f8a0f69f96ef4ff3a2a319e1477d4a86ffcb2
Lulakshi Hettiarachchi <lulakshinadunihettiarachchi@gmail.com>
2022-08-10 23:47:01 +0530

    This commit is for astrologer dash board stat card backend

--- query/Astrologer/al_dash_board.js
+++ query/Astrologer/al_dash_board.js
@@ -1,2 +1,5 @@
 exports.PROFILE_card = "SELECT * FROM `astrologer` WHERE `astrologer`.`user_id` = 1 ";
 // http://localhost:3000/al/db/profile_card/?user_id=1
+
+exports.STAT_card1 = "SELECT COUNT(*) AS waiting FROM astrologer_request WHERE request_status=0";
+exports.STAT_card2 = "SELECT COUNT(*) AS replied FROM astrologer_request WHERE request_status=1";
