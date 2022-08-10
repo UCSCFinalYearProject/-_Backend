b2ddb6399453bf8d73f5cc02b075e50088b7df89
akilaanjanadissanayaka <akilaanjanadissanayaka@gmail.com>
2022-08-10 14:43:23 +0530

    This commit for MA-33 Admin_View_Pediatrician_List_Backend

--- route/Admin/admin_routs.js
+++ route/Admin/admin_routs.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mother_route = express.Router();
 const mother_controller = require('../../controller/Admin/Mother/admin_mother_controller');
 const astrologer_controller = require('../../controller/Admin/Astrologer/astrologer_controller');
+const paediatrician_controller = require('../../controller/Admin/Pediatrician/pediatrician_controller');
 
 // Thisrouts for mother
 mother_route.get("/mother_list",mother_controller.registered_mothers);
@@ -23,6 +24,11 @@ mother_route.get("/unblock_mother",mother_controller.Unblock_Mother);
 mother_route.get("/astrologerlist",astrologer_controller.registered_astrologers);
 mother_route.get("/block_astrologer",astrologer_controller.Block_astrologers);
 mother_route.get("/unblock_astrologer",astrologer_controller.Unblock_astrologers);
+
+// This routs for paediatrician
+mother_route.get("/paediatricianlist",paediatrician_controller.registered_pediatrician);
+mother_route.get("/block_paediatrician",paediatrician_controller.Block_pediatrician);
+mother_route.get("/unblock_paediatrician",paediatrician_controller.Unblock_pediatrician);
 
 
 
