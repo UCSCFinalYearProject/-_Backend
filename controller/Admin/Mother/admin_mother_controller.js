5a160a32e23ab8dbc1ddbd249bee90577e72d650
akilaanjanadissanayaka <akilaanjanadissanayaka@gmail.com>
2022-08-10 12:40:07 +0530

    This commit for MA-31 Admin_View_Mothers _List_Backend

--- controller/Admin/Mother/admin_mother_controller.js
+++ controller/Admin/Mother/admin_mother_controller.js
@@ -1,17 +1,25 @@
 const { isEmpty } = require('../../../utils/is_empty');
 const conn = require('../../../service/db_service');
-const { REGISTERED_Mothers } = require('../../../query/Admin/Mother/admin_mother');
+const {
+    REGISTERED_Mothers,
+    NumOfPosts,
+    NumOfComments,
+    BlockMother,
+    UnblockMother,
+    MotherPostsList,
+    MotherPostDetails,
+    MotherPostReplyCount,
+    PostComments
+} = require('../../../query/Admin/Mother/admin_mother');
 const { MOTHER_MODEL} = require('../../../model/Mother/mother_model');
 const bcrypt = require('bcryptjs');
 const AppError = require('../../../utils/appError');
 const JWT = require('jsonwebtoken');
 
-
 exports.registered_mothers = (req, res, next) => {
-    // if (isEmpty(req.body)) return next( new AppError("form data not found" , 400));
     try {
-        // const { error } = MOTHER_MODEL.validate(req.body);
-        // if (error) return next( new AppError( error.details[0].message , 400));
+        const { error } = MOTHER_MODEL.validate(res.body);
+        if (error) return next( new AppError( error.details[0].message , 400));
 
         conn.query(REGISTERED_Mothers, (err,data,feild)=>{
 
@@ -20,8 +28,7 @@ exports.registered_mothers = (req, res, next) => {
             }
             else{
                 res.status(200).json({
-                    students:data,
-                    data:"success"
+                    students:data
                 })
             }
 
@@ -29,8 +36,149 @@ exports.registered_mothers = (req, res, next) => {
     } catch ( err ) {
 
     }
-    // res.status(200).json({
-    //     // students:data
-    //     data:"success"
-    // })
+}
+exports.NOF_Posts = (req, res, next) => {
+    try {
+
+        conn.query(NumOfPosts,[req.body.uid], (err,data,feild)=>{
+
+            if(err){
+                return next(new AppError(err))
+            }
+            else{
+                res.status(200).json({
+                    students:data
+                })
+            }
+
+        })
+    } catch ( err ) {
+
+    }
+}
+exports.Mother_Posts_list = (req, res, next) => {
+    try {
+
+        conn.query(MotherPostsList,[req.body.uid], (err,data,feild)=>{
+
+            if(err){
+                return next(new AppError(err))
+            }
+            else{
+                res.status(200).json({
+                    students:data
+                })
+            }
+
+        })
+    } catch ( err ) {
+
+    }
+}
+exports.NOF_Comments = (req, res, next) => {
+    try {
+        conn.query(NumOfComments,[req.body.uid],  (err,data,feild)=>{
+
+            if(err){
+                return next(new AppError(err))
+            }
+            else{
+                res.status(200).json({
+                    students:data
+                })
+            }
+
+        })
+    } catch ( err ) {
+
+    }
+}
+
+// block mother
+exports.Block_Mother = (req, res, next) => {
+    try {
+        conn.query(BlockMother,[req.body.uid],  (err,data,feild)=>{
+
+            if(err){
+                return next(new AppError(err))
+            }
+            else{
+                res.status(200).json({
+                    message:"success"
+                })
+            }
+
+        })
+    } catch ( err ) {
+
+    }
+}
+// unblock mother
+exports.Unblock_Mother = (req, res, next) => {
+    try {
+        conn.query(UnblockMother,[req.body.uid],  (err,data,feild)=>{
+
+            if(err){
+                return next(new AppError(err))
+            }
+            else{
+                res.status(200).json({
+                    message:"success"
+                })
+            }
+        })
+    } catch ( err ) {
+
+    }
+}
+exports.Mother_Post_Details = (req, res, next) => {
+    try {
+        conn.query(MotherPostDetails,[req.body.pid],  (err,data,feild)=>{
+
+            if(err){
+                return next(new AppError(err))
+            }
+            else{
+                res.status(200).json({
+                    data:data
+                })
+            }
+        })
+    } catch ( err ) {
+
+    }
+}
+exports.Mother_Post_Reply_Count = (req, res, next) => {
+    try {
+        conn.query(MotherPostReplyCount,[req.body.pid],  (err,data,feild)=>{
+
+            if(err){
+                return next(new AppError(err))
+            }
+            else{
+                res.status(200).json({
+                    data:data
+                })
+            }
+        })
+    } catch ( err ) {
+
+    }
+}
+exports.Post_Comments_List = (req, res, next) => {
+    try {
+        conn.query(PostComments,[req.body.pid],  (err,data,feild)=>{
+
+            if(err){
+                return next(new AppError(err))
+            }
+            else{
+                res.status(200).json({
+                    data:data
+                })
+            }
+        })
+    } catch ( err ) {
+
+    }
 }
--- route/Admin/admin_routs.js
+++ route/Admin/admin_routs.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const mother_route = express.Router();
+const astrologer_routs=express.Router();
 const mother_controller = require('../../controller/Admin/Mother/admin_mother_controller');
 
+// Thisrouts for mother
+mother_route.get("/mother_list",mother_controller.registered_mothers);
+// send uid with req body
+mother_route.get("/mother_nof_post",mother_controller.NOF_Posts);
+mother_route.get("/mother_posts_list",mother_controller.Mother_Posts_list);
+mother_route.get("/post_comments_list",mother_controller.Post_Comments_List);
 
-mother_route.get("/",mother_controller.registered_mothers);
-mother_route.route("/")
-    .get(mother_controller.registered_mothers)
+mother_route.get("/Mother_Post_Details",mother_controller.Mother_Post_Details);
+mother_route.get("/Mother_Post_Reply_Count",mother_controller.Mother_Post_Reply_Count);
+
+// send uid with req body
+mother_route.get("/mother_nof_comments",mother_controller.NOF_Comments);
+mother_route.get("/block_mother",mother_controller.Block_Mother);
+mother_route.get("/unblock_mother",mother_controller.Unblock_Mother);
+
+
+// This routs for Astrologer
+astrologer_routs.get("/mother_nof_comments",mother_controller.NOF_Comments);
+
+
+
 
 module.exports = mother_route;
