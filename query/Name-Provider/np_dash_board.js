419adf0ca399b9cb426f5b0266382b4d39cf5a1d
Lulakshi Hettiarachchi <lulakshinadunihettiarachchi@gmail.com>
2022-08-10 23:57:00 +0530

    This commit is for name provider dash board stat card backend

--- query/Name-Provider/np_dash_board.js
+++ query/Name-Provider/np_dash_board.js
@@ -1,2 +1,5 @@
 exports.PROFILE_card = "SELECT * FROM `baby_name_provider` WHERE `user_id` = ? " ;
 // http://localhost:3000/np/db/profile_card/?user_id=1
+
+exports.STAT_card1 = "SELECT COUNT(*) AS waiting FROM baby_name_provider_request WHERE request_status=0";
+exports.STAT_card2 = "SELECT COUNT(*) AS replied FROM baby_name_provider_request WHERE request_status=1";
