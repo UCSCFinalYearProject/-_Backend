8e91869d680efbe2d364df426704cb02bda7dc2e
Lulakshi Hettiarachchi <lulakshinadunihettiarachchi@gmail.com>
2022-08-10 22:45:09 +0530

    This commit is for astrologer dash board profile card backend

---
+++ query/Astrologer/al_dash_board.js
@@ -0,0 +1,2 @@
+exports.PROFILE_card = "SELECT * FROM `astrologer` WHERE user_id = ? " ;
+// http://localhost:3000/al/db/profile_card/?user_id=1
--- route/Astrologer/al_routs.js
+++ route/Astrologer/al_routs.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const al_request_table_route = express.Router();
+const al_route = express.Router();
 const al_request_table_controller = require('../../controller/Astrologer/al_request_table_controller.js');
+const profile_card_controller = require('../../controller/Astrologer/Al-Dashboard/profile_card_controller')
+
+al_route.get("/new_request",al_request_table_controller.request_table);
+al_route.get("/db/profile_card",profile_card_controller.profile_card);
 
 
-al_request_table_route.get("/new_request",al_request_table_controller.request_table);
-al_request_table_route.route("/new_request")
-    .get(al_request_table_controller.request_table)
-
-module.exports = al_request_table_route;
+module.exports = al_route;
