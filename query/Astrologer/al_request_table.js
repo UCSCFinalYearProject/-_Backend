6a4a808fff4e3e04429fe41fb55091d199a9ec4f
MadhuniT <2019cs168@stu.ucsc.lk>
2022-08-16 18:41:03 +0530

    add folder structure

--- query/Astrologer/al_request_table.js
+++
@@ -1,3 +0,0 @@
-exports.REQUEST_table = "SELECT *" +
-                        " FROM parent a ,astrologer_request b " +
-                        "WHERE a.user_id =b.requester_id AND b.receiver_id=?" ;
