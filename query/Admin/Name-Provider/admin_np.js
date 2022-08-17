753dd25663f95028c8835d5cf8c94d9ceaaa379c
Lulakshi Hettiarachchi <lulakshinadunihettiarachchi@gmail.com>
2022-08-17 14:30:25 +0530

    This commit is for name provider report stat card 1,2 backend

--- query/Admin/Name-Provider/admin_np.js
+++
@@ -1,5 +0,0 @@
-exports.REGISTERED_Astrologers = " SELECT * FROM `astrologer`" ;
-exports.Block_Astrologers ="UPDATE `astrologer` SET `STATUS` = '1' WHERE `astrologer`.`user_id` = ?"
-exports.Unblock_Astrologers ="UPDATE `astrologer` SET `STATUS` = '0' WHERE `astrologer`.`user_id` = ?"
-
-
