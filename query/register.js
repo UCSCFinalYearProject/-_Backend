a555994135e86b44993fc8d1fd8e92960e743166
MadhuniT <2019cs168@stu.ucsc.lk>
2022-09-14 05:53:20 +0530

    this commit for pediatrician view today comment backend

--- query/register.js
+++ query/register.js
@@ -11,3 +11,11 @@ exports.CHECK_user_al_email = "SELECT * FROM astrologer WHERE email = ?";
 exports.CHECK_user_np_email = "SELECT * FROM baby_name_provider WHERE email = ?";
 exports.EXTRA_TABLE = "INSERT INTO `common_user_details` VALUES ( ?,?,?,)";
 
+
+//login queries
+
+exports.CHECK_EMAIL = "SELECT * FROM `common_user_details` WHERE email = ?";
+exports.CHECK_NP = "SELECT * FROM `baby_name_provider` WHERE email = ?";
+exports.CHECK_AL = "SELECT * FROM `astrologer` WHERE email = ?";
+exports.CHECK_PT = "SELECT * FROM `paediatrician` WHERE email = ?";
+exports.CHECK_ADMIN = "SELECT * FROM `admin` WHERE email = ?";
