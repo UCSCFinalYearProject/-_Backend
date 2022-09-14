e29fabe516f23231a05cacd5976b69b76fd8cb32
Lulakshi Hettiarachchi <lulakshinadunihettiarachchi@gmail.com>
2022-09-14 23:34:14 +0530

    This commit is for np dash board backend completed

--- query/Astrologer/al_dash_board.js
+++ query/Astrologer/al_dash_board.js
@@ -7,3 +7,5 @@ exports.STAT_card2 = "SELECT COUNT(*) AS replied FROM astrologer_request WHERE r
 exports.REQUEST_chart = "select month(request_date), count(*) from astrologer_request WHERE year(request_date) = '2022'  AND receiver_id=? group by month(request_date)";
 
 exports.PROFIT_chart="select month(date_time), sum(amount) from astrologer_payment a, astrologer_request b WHERE year(date_time) = '2022' AND b.receiver_id=? AND a.request_id=b.request_id group by month(date_time)";
+
+exports.TODAY_req="SELECT * FROM parent a ,astrologer_request b WHERE a.user_id =b.requester_id AND b.receiver_id=? AND date(b.request_date) = CURRENT_DATE";
--- route/Astrologer/al_routs.js
+++ route/Astrologer/al_routs.js
@@ -9,6 +9,8 @@ const profit_chart_controller = require('../../controller/Astrologer/Al-Dashboar
 
 
 al_route.get("/new_request/:receiver_id",al_request_table_controller.request_table);
+
+al_route.get("/db/today_request/:receiver_id",al_request_table_controller.today_request_table);
 al_route.get("/db/profile_card/:user_id",profile_card_controller.profile_card);
 al_route.get("/db/stat_card1/:receiver_id",stat_card1_controller.stat_card);
 al_route.get("/db/stat_card2/:receiver_id",stat_card2_controller.stat_card);
