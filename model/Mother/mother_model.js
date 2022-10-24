0c253255e27278244af940adb9b52312be36290b
Hansana Ranaweera <73303406+ranaHP@users.noreply.github.com>
2022-10-24 16:51:35 +0530

    This Commit for complete mother login API Endpoint

--- model/Mother/mother_model.js
+++ model/Mother/mother_model.js
@@ -10,10 +10,10 @@ exports.MOTHER_MODEL = Joi.object({
 
 exports.PARENT_MODEL = Joi.object({
     user_id: Joi.string(),
-    first_name: Joi.string().min(3).max(100).required(),
-    last_name: Joi.string().min(3).max(100).required(),
-    email: Joi.string().min(3).max(200).required(), 
-    password: Joi.string().min(8).max(100).required(),
+    first_name: Joi.string().min(3).max(100),
+    last_name: Joi.string().min(3).max(100),
+    email: Joi.string().min(3).max(200), 
+    password: Joi.string().min(8).max(100),
     mobile: Joi.string(),
     DP: Joi.string(),
     STATUS: Joi.string(),
